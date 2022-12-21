9e080846d1b7fbd7a5377f7327df688f89ea246c
HsuMonLynn <scm.hsumonlynn@gmail.com>
2022-12-21 14:45:20 +0630

    reaction timer

---
+++ src/components/Block.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer"> Click here {{ delay }}
+  </div>
+</template>
+
+<script>
+export default {
+   props:['delay'],
+   data(){
+      return {
+          showBlock:false,
+          score:0,
+          timer:null
+      }
+  },
+   mounted(){
+      setTimeout(() => {
+          this.showBlock=true;//updade
+          this.startTimer()
+      }, this.delay);
+  },
+  methods:{
+    startTimer(){
+      this.timer = setInterval(()=>{
+        this.score+=50;
+      },50);
+    } ,
+    stopTimer(){
+      clearInterval(this.timer);  
+      this.$emit("endGame",this.score)
+    }
+  }
+}
+</script>
+
+<style>
+.block{
+    background-color: aqua;
+    margin: 20px auto;
+    width: 200px;
+    height: 200px;
+    padding-top: 20px;
+}
+</style>
